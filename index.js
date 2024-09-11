eea74e421bd94ae022f4fed435ca376dc649d307
akpobasa12345 <victorakpobasa2020@gmail.com>
2024-09-11 21:42:07 +0100

    initial commit

--- index.js
+++ index.js
@@ -2,3 +2,45 @@
 window.addEventListener("scroll", () => {
    document.querySelector("nav").classList.toggle("window-scroll", scrollY > 30)
 })
+
+
+const faqs = document.querySelectorAll(".faq")
+
+faqs.forEach(faq => {
+   faq.addEventListener("click", () => {
+
+      faq.classList.toggle("open")
+
+      const faqIcon = document.querySelector(".faq i")
+      if(faqIcon.className === "fa-solid fa-plus"){
+faqIcon.className === "fa-solid fa-minus"
+      } else {
+         faqIcon.className === "fa-solid fa-plus"
+
+      }
+   })
+})
+
+
+const menu = document.querySelector(".menu")
+const closeMenuIcon = document.querySelector(".close-menu")
+const openMenuIcon = document.querySelector(".open-menu")
+
+
+
+openMenuIcon.addEventListener("click", () => {
+   menu.style.display = "block";
+   closeMenuIcon.style.display = "block";
+   openMenuIcon.style.display = "none";
+})
+
+
+closeMenuIcon.addEventListener("click", () => {
+   menu.style.display = "none";
+   closeMenuIcon.style.display = "none";
+   openMenuIcon.style.display = "block";
+})
+
+
+
+
